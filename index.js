3d0ccc34f5f30c6042cc1560b72edbdb4822d2ce
Miguel Castillo <manchagnu@gmail.com>
2015-03-02 22:46:42 -0500

    refactored name of the input variable and method that runs the magnification

--- index.js
+++ index.js
@@ -1,35 +1,14 @@
-'use strict';
-
 var uglify = require('uglify-js');
 var merge  = require('utils-merge');
 
 
 /**
- * Method that executes the minification step
- */
-function _minify(item, options){
-  options = options || {};
-  if (options.sourceMap) {
-    options.outSourceMap = item.name ? item.name + '.map' : 'out.js.map';
-  }
-
-  var result = uglify.minify(item.source, merge({fromString: true}, options));
-  item.source = result.code;
-
-  // If sourcemap information is generated, let's just copy it in the item
-  if (result.map) {
-    item.map = result.map;
-  }
-}
-
-
-/**
  * Method to minify source
  *
- * @param {{source: string}} item - Object with a `source` property to be minified
+ * @param {{source: string}} data - Object with a `source` property to be minified
  */
-function minify(item) {
-  return _minify(item);
+function minify(data) {
+  return _run(data);
 }
 
 
@@ -41,10 +20,29 @@ function minify(item) {
  * @returns {function} delegate to be called with object with `source` property to be minified
  */
 minify.config = function(options) {
-  return function minify(item) {
-    _minify(item, options);
+  return function minify(data) {
+    _run(data, options);
   };
 };
+
+
+/**
+ * Method that executes the minification step
+ */
+function _run(data, options){
+  options = options || {};
+  if (options.sourceMap) {
+    options.outSourceMap = data.name ? data.name + '.map' : 'out.js.map';
+  }
+
+  var result = uglify.minify(data.source, merge({fromString: true}, options));
+  data.source = result.code;
+
+  // If sourcemap information is generated, let's just copy it into the data
+  if (result.map) {
+    data.map = result.map;
+  }
+}
 
 
 module.exports = minify;
